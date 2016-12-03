5ada66a0ac0a28fbff2b34003313fe2926ce59af
w205 <w205@ip-172-31-10-210.ec2.internal>
2016-12-03 20:21:19 +0000

    Adding location sqls from Scott

---
+++ scripts/user_loc.sql
@@ -0,0 +1,18 @@
+
+create table user_loc as select
+    u.user_id as user_id
+    ,r.latitude as latitude
+    ,r.latitude - 0.01 as min_latitude
+    ,r.latitude + 0.01 as max_latitude
+    ,r.longitude as longitude
+    ,r.longitude - 0.01 as min_longitude
+    ,r.longitude + 0.01 as max_longitude
+    
+from 
+    yelp_user u 
+    left join
+    review_loc r
+
+on u.user_id = r.user_id
+
+where r.latitude is not null;
